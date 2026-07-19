80c7fd1ad43bb2a2760aa1350c8716ae4db879dd
COZYkrish <cozykrish2916@gmail.com>
2026-07-19 12:00:00 +0530

    Your commit message

--- server/src/modules/gamification/gamification.controller.ts
+++ server/src/modules/gamification/gamification.controller.ts
@@ -25,7 +25,6 @@ export const getAchievements = async (req: AuthRequest, res: Response, next: Nex
     next(error);
   }
 };
-
 export const getChallenges = async (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
     const userId = req.user?.id;
@@ -38,17 +37,17 @@ export const getChallenges = async (req: AuthRequest, res: Response, next: NextF
   }
 };
 
-export const getDailyGoal = async (req: AuthRequest, res: Response, next: NextFunction) => {
-  try {
-    const userId = req.user?.id;
-    if (!userId) throw Object.assign(new Error('Unauthorized'), { statusCode: 401 });
+// export const getDailyGoal = async (req: AuthRequest, res: Response, next: NextFunction) => {
+//   try {
+//     const userId = req.user?.id;
+//     if (!userId) throw Object.assign(new Error('Unauthorized'), { statusCode: 401 });
 
-    const goal = await gamificationService.getDailyGoal(userId);
-    res.status(200).json({ success: true, data: goal });
-  } catch (error) {
-    next(error);
-  }
-};
+//     const goal = await gamificationService.getDailyGoal(userId);
+//     res.status(200).json({ success: true, data: goal });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 export const recalculateXP = async (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
@@ -62,7 +61,7 @@ export const recalculateXP = async (req: AuthRequest, res: Response, next: NextF
       action: 'MANUAL_RECALCULATION',
       xpAmount: 0
     });
-    
+
     res.status(200).json({ success: true, data: result });
   } catch (error) {
     next(error);
